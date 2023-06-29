d5c1bf2c7672e694179da7b87ec059337f1a4afb
Anastasia <talanovanastya@gmail.com>
2023-06-29 20:53:06 +0300

    last commit

---
+++ задача13/Program.cs
@@ -0,0 +1,18 @@
+﻿//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+
+Console.Clear();
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a < 99){
+    Console.WriteLine("Это не трехзначное число");
+    return;
+}
+while(a > 999){
+    a /= 10;
+}
+int result = a%10;
+Console.WriteLine($"->{result}");
---
+++ задача15/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+//6 -> да
+//7 -> да
+//1 -> нет
+
+Console.Clear();
+Console.WriteLine("Введите номер дня недели: ");
+int numberOfWeekDay = int.Parse(Console.ReadLine());
+
+if (numberOfWeekDay > 5 && numberOfWeekDay < 8){
+    Console.WriteLine("Это выходной");
+}
+if (numberOfWeekDay >= 1 && numberOfWeekDay < 6){
+    Console.WriteLine("Это будний день");
+}
+if (numberOfWeekDay > 7){
+    Console.WriteLine("Это не день недели");
+}
+
+
